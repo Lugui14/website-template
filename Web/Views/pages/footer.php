9e28831f1d54950c8649c8a2c844d65481b4917b
Luiz Guilherme Zanella Lopes <zanelallopes9977@gmail.com>
2021-04-15 14:20:44 -0300

    Home Page

---
+++ Web/Views/pages/footer.php
@@ -0,0 +1,31 @@
+<footer>
+
+  <link rel="stylesheet" href="<?php echo INCLUDE_PATH_STATIC ?>public/css/footer/footer.css">
+
+  <div class="dividertop">
+    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
+    </svg>
+  </div>
+
+  <div class="content">
+    <div class="nav">
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Costumers</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </div>
+
+    <div class="credits">
+      <p>This website was made by <b>Luiz Guilherme</b></p>
+
+      <p>Need help? <b>Contact Us</b></p>
+    </div>
+
+  </div>
+
+  <p class="terms">Terms of Service - Privacy Policy</p>
+</footer>
--- Web/Views/pages/home.php
+++ Web/Views/pages/home.php
@@ -58,25 +58,63 @@
 
     <div class="cards">
       <div class="card">
-        <h3>Service</h3>
+        <h3>Service 1</h3>
 
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-      </div><!--card-->
+      </div><!--card1-->
+
+      <div class="card">
+        <h3>Service 2</h3>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div><!--card2-->
+
+      <div class="card">
+        <h3>Service 3</h3>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div><!--card3-->
+
+      <div class="card">
+        <h3>Service 4</h3>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div><!--card4-->
+
+      <div class="card">
+        <h3>Service 5</h3>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div><!--card5-->
+
+      <div class="card">
+        <h3>Service 6</h3>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div><!--card6-->
     </div><!--cards-->
   </section>
 
 
   <section class='costumers-section'>
     <div class="content">
-      <span>more than</span>
-      <span>100</span>
-      <span>satisfied customers</span>
+      <span>more than <br><b>100</b><br> satisfied customers</span>
     </div><!--content-->
   </section>
 
 
   <section class='phrase-section'>
+    <div class="dividertop">
+    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
+    </svg>
+    </div>
     <h1>Lorem ipsum dolor sit amet.</h1>
+    <div class="dividerbottom"> 
+    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+    </svg>
+  </div>
   </section>
 
   
@@ -96,6 +134,9 @@
     </div><!--slider-->
   </section>
 
+
+  <?php include("footer.php"); ?>
+  
   <script src="https://kit.fontawesome.com/560a2dac7b.js" crossorigin="anonymous"></script>
 </body>
 </html>
